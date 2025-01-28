bab7369023b65cb04d480d2e316af49bc5403477
f-yusei <fkys2932@gmail.com>
2025-01-28 11:57:23 +0900

    feat: TLS設定を追加し、環境変数からポートを取得するように変更

--- infrastructure/mysql/db.go
+++ infrastructure/mysql/db.go
@@ -1,28 +1,39 @@
 package infrastructure
 
 import (
+	"crypto/tls"
 	"fmt"
 	"gcp_go_cloud_run/app/infrastructure/mysql/entity"
 	"log"
 	"os"
 
-	"gorm.io/driver/mysql"
+	gormsql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		getEnv("DB_USER", "root"),
-		getEnv("DB_PASSWORD", ""),
-		getEnv("DB_HOST", "localhost"),
-		getEnv("DB_PORT", "4000"),
-		getEnv("DB_NAME", "your_db_name"),
+
+	err := mysql.RegisterTLSConfig("tidb", &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		ServerName: GetEnv("DB_HOST", "localhost"),
+	})
+	if err != nil {
+		log.Fatalf("failed to register TLS config: %v", err)
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&tls=tidb",
+		GetEnv("DB_USER", "root"),
+		GetEnv("DB_PASSWORD", ""),
+		GetEnv("DB_HOST", "localhost"),
+		GetEnv("DB_PORT", "4000"),
+		GetEnv("DB_NAME", "your_db_name"),
 	)
 
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(gormsql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
 	}
@@ -30,10 +41,14 @@ func InitDB() {
 	DB.AutoMigrate(&entity.Bell{}, &entity.Store{}, &entity.CallLog{})
 }
 
-func getEnv(key string, defaultValue string) string {
+func GetEnv(key string, defaultValue string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists {
 		return defaultValue
 	}
 	return value
+}
+
+func DebugPrint(v interface{}) {
+	fmt.Printf("%+v\n", v)
 }
